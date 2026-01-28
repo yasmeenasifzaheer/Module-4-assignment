cec2c5607a1c043783e3ef2137a7bdfefcb0553c
yasmeenasif <zaheeryasmeen05@gmail.com>
2026-01-28 11:41:10 +0530

    modified file

--- server/app.js
+++ server/app.js
@@ -13,5 +13,9 @@ app.use(cors());
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/tasks", require("./routes/taskRoutes"));
 
-const PORT = process.env.PORT || 5000;
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
+
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- server/config/db.js
+++ server/config/db.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB Connected");
+    console.log("MongoDB connected");
   } catch (error) {
-    console.error(error);
+    console.error(error.message);
     process.exit(1);
   }
 };
